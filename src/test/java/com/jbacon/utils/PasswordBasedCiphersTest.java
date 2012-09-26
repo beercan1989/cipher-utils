34eff4d3ab03bc30bdda519fb36c7dd8d4b2495e
James Bacon <cheese1989@gmail.com>
2012-09-26 21:33:03 +0100

    First commit to project after creation.

---
+++ src/test/java/com/jbacon/utils/PasswordBasedCiphersTest.java
@@ -0,0 +1,14 @@
+package com.jbacon.utils;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class PasswordBasedCiphersTest {
+
+    @Test
+    public void shouldBeAbleToEncryptWithPbeWithMD5AndDes() {
+        fail("Not yet implemented");
+    }
+
+}
