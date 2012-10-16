eb696003c987560b89716cd7ff9bd7edd00f63db
James Bacon <james.bacon@technophobia.com>
2012-10-16 08:36:39 +0100

    Changed, can't remember for what.

--- src/test/java/com/jbacon/cryptography/utils/CipherUtils.java
+++ src/test/java/com/jbacon/cryptography/utils/CipherUtils.java
@@ -18,10 +18,8 @@ public final class CipherUtils {
     /**
      * Converts a UTF-8 byte array into a Base64 Encoded String.
      * 
-     * @param bytes
-     *            The UTF-8 Encoded byte array.
-     * @return A String containing the Base64 Encoded values of the UTF-8 byte array or <strong>null</strong> if the
-     *         <strong>bytes</strong> param is <strong>null</strong>.
+     * @param bytes The UTF-8 Encoded byte array.
+     * @return A String containing the Base64 Encoded values of the UTF-8 byte array or <strong>null</strong> if the <strong>bytes</strong> param is <strong>null</strong>.
      */
     public static final String bytesToBase64Encoded(final byte[] bytes) {
         if (bytes == null) {
@@ -34,10 +32,8 @@ public final class CipherUtils {
     /**
      * Converts a Base64 Encoded String into a UTF-8 Encoded byte array.
      * 
-     * @param base64String
-     *            The Base64 Encoded String.
-     * @return A UTF-8 byte array of the Decoded Base64 String or <strong>null</strong> if the
-     *         <strong>base64String</strong> param is <strong>null</strong>.
+     * @param base64String The Base64 Encoded String.
+     * @return A UTF-8 byte array of the Decoded Base64 String or <strong>null</strong> if the <strong>base64String</strong> param is <strong>null</strong>.
      */
     public static final byte[] base64EncodedStringToBytes(final String base64String) {
         if (base64String == null) {
@@ -50,10 +46,8 @@ public final class CipherUtils {
     /**
      * Converts a UTF-8 byte array into a String.
      * 
-     * @param bytes
-     *            The UTF-8 byte array to covnert into a String.
-     * @return A String made from the UTF-8 byte array or <strong>null</strong> if the <strong>bytes</strong> param is
-     *         <strong>null</strong>.
+     * @param bytes The UTF-8 byte array to covnert into a String.
+     * @return A String made from the UTF-8 byte array or <strong>null</strong> if the <strong>bytes</strong> param is <strong>null</strong>.
      */
     public static final String byteToString(final byte[] bytes) {
         if (bytes == null) {
@@ -70,10 +64,8 @@ public final class CipherUtils {
     /**
      * Converts a String to a UTF-8 encoded byte array.
      * 
-     * @param string
-     *            The String to convert into a UTF-8 byte array.
-     * @return A UTF-8 encoded byte array or <strong>null</strong> if the <strong>string</strong> param is
-     *         <strong>null</strong>.
+     * @param string The String to convert into a UTF-8 byte array.
+     * @return A UTF-8 encoded byte array or <strong>null</strong> if the <strong>string</strong> param is <strong>null</strong>.
      */
     public static final byte[] stringToByte(final String string) {
         try {
@@ -90,8 +82,7 @@ public final class CipherUtils {
      * 
      * @param numberOfBytes
      * @return
-     * @throws NoSuchAlgorithmException
-     *             This is thrown if their isn't a provider for the 'SHA1PRNG'
+     * @throws NoSuchAlgorithmException This is thrown if their isn't a provider for the 'SHA1PRNG'
      */
     public static final byte[] generateBytes(final int numberOfBytes) {
         final byte[] salt = new byte[numberOfBytes];
